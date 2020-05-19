7aa1446399d6276fcb52d4bc61f739d95fc6f5a8
andrew <andrewnaiemf@gmail.com>
2020-05-19 20:04:55 +0200

    first commitdashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.dashboard.app')
+
+
+@section('content')
+
+    <div class="container" style="justify-content: center;width: 900px; min-height: 566px">
+        <h1>
+            @lang('site.dashboard')
+        </h1>
+    </div>
+
+    @endsection
